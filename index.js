884cb1bde79f66a4e81d11a8cc170e3f705bdce3
Kashinath Patekar <42115482+stellarbeam@users.noreply.github.com>
2021-06-12 14:27:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/:name', (req, res) {
+      var name = req.params.name;
+      console.log(JSON.stringify(req.headers));
+      res.redirect(`https://touch-msg.com/in/f-ipd?f=${name}`)
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
