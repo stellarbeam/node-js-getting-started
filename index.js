3ec111295e0d2cce449680fe9eadf88a4b08486b
Kashinath Patekar <42115482+stellarbeam@users.noreply.github.com>
2021-06-12 14:36:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/:name', (req, res) {
-      var name = req.params.name;
-      console.log(JSON.stringify(req.headers));
-      res.redirect(`https://touch-msg.com/in/f-ipd?f=${name}`)
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    res.redirect("/banner-dashboard")
+});
+
+app.listen(process.env.PORT || 80, () => {
+    console.log('Server started!');
+});
+
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/:name', (req, res) {
+//       var name = req.params.name;
+//       console.log(JSON.stringify(req.headers));
+//       res.redirect(`https://touch-msg.com/in/f-ipd?f=${name}`)
+//   })
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
